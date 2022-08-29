ce6dd00bea5068813e54f783e662033ffe100823
Jake Ashton <jakemaddenashton@gmail.com>
2022-08-29 15:53:04 -0700

    Create module function to add marks on the board

--- javascript.js
+++ javascript.js
@@ -4,6 +4,14 @@ const gameBoard = (() => {
 })();
 
 const displayController = (() => {
+    const squares = document.querySelectorAll("div[data-number]");
+    const gameMove = squares.forEach(square => {
+        square.addEventListener('click', () => {
+            square.textContent = 'x';
+            gameBoard.splice(square.dataset.number, 0, square.textContent);
+        });
+    });
+    return {gameMove};
 })();
 
 const Player = (name) => {
